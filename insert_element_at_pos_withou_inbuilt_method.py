5e0b7a8027abea25bed9936f613d8f74aaa56b06
Akash Ilanthendral T <37402228+akashilan@users.noreply.github.com>
2025-06-21 12:13:31 +0530

    Create insert_element_at_pos_withou_inbuilt_method.py
    First commit. Just added the file

---
+++ insert_element_at_pos_withou_inbuilt_method.py
@@ -0,0 +1,8 @@
+def insertElement(list1, pos, ele):
+    """This function inserts an element(ele) in a list(list1) at the given position(pos)"""
+  
+    list1.append(list1[-1])
+    for i in range(len(list1)-2, pos-1, -1):
+        list1[i+1] = list1[i]
+        
+    list1[pos] = ele
