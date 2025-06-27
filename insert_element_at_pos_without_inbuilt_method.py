978dc95e29c4f009071e8385f15cb1994fd812ea
Akash Ilanthendral T <37402228+akashilan@users.noreply.github.com>
2025-06-28 01:11:26 +0530

    Update and rename insert_element_at_pos_withou_inbuilt_method.py to insert_element_at_pos_without_inbuilt_method.py

--- insert_element_at_pos_without_inbuilt_method.py
+++ insert_element_at_pos_without_inbuilt_method.py
@@ -1,5 +1,5 @@
 def insertElement(list1, pos, ele):
-    """This function inserts an element(ele) in a list(list1) at the given position(pos)"""
+    """This function inserts an element(ele) in a list(list1) at the given position(pos) without using inbuilt methods."""
   
     list1.append(list1[-1])
     for i in range(len(list1)-2, pos-1, -1):
