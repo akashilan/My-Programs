2a54d2c579a5dec6adc45808d498c9e1272e1fdb
Akash Ilanthendral T <37402228+akashilan@users.noreply.github.com>
2025-07-09 02:10:35 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,27 +1,25 @@
 def gcd(n, m):
 	if n == m:
 		return n
+
 	if (n ==0 and m != 0) or (m==0 and n!=0):
-		return m if m!=0 else n
+		return abs(m) if m!=0 else abs(n)
+
 	if n == 0 and m == 0:
 		return '0 or undefined'
 	
 	small, big = 0, 0
 	if n < m:
-		small = n
-		big = m
+		small, big= n, m
 	else:
-		small = m
-		big = n
-	
+		small, big = m, n
+  	
 	if big % small == 0:
 		return small
-	
-	gcd_factor = 1
-	for i in range(1, small+1):
+
+	for i in range(small, 0, -1):
 		if small % i == 0 and big % i == 0:
-			gcd_factor = i
-	return gcd_factor
+			return i
 
 
 should_continue = 'y'
@@ -29,4 +27,4 @@ while(should_continue == 'y'):
 	n = int(input("Enter n: "))
 	m = int(input("Enter m:"))
 	print(gcd(n,m))
-	should_continue = input("Want to continue? : (y/n) ")
+	should_continue = input("Want to continue? : (y/n) ").lower()
